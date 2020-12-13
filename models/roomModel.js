f27ff99a2731d58697dfbd15f30ded0fc69ede2e
Yuhuan(Evan) Zhou <yzhou244@myseneca.ca>
2020-12-13 13:33:12 -0500

    update

--- models/roomModel.js
+++ models/roomModel.js
@@ -26,35 +26,6 @@ const roomSchema = new Schema({
   },
   "thumbnail": {
     type: String,
-    unique: true
-  },
-  "upload1": {
-    type: String,
-    unique: true
-  },
-  "upload2": {
-    type: String,
-    unique: true
-  },
-  "upload3": {
-    type: String,
-    unique: true
-  },
-  "upload4": {
-    type: String,
-    unique: true
-  },
-  "master": {
-    type: String,
-    unique: true
-  },
-  "second": {
-    type: String,
-    unique: true
-  },
-  "third": {
-    type: String,
-    unique: true
   },
   "type": String,
   "moreComment": String
